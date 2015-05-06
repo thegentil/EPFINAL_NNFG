006b901f011a89fbf9950e064e4fd643f92e85dc
thegentil <nicogentil@gmail.com>
2015-05-06 16:53:44 -0300

    Versão 4

--- Projeto Final - Main.py
+++ Projeto Final - Main.py
@@ -8,11 +8,16 @@ Projeto Final - Mapa virtual
 
 """
 
-Nomes_A = open("Turma_A_Nomes", encoding="utf-8")
-Usuários_A = open("Turma_A_Usuários", encoding="utf-8")
+Arquivo_Nomes_A = open("Turma_A_Nomes.txt", encoding="utf-8")
+Arquivo_Usuários_A = open("Turma_A_Usuários.txt", encoding="utf-8")
 
-Nomes_B = open("Turma_B_Nomes", encoding="utf-8")
-Usuários_B = open("Turma_B_Usuários", encoding="utf-8")
+Arquivo_Nomes_B = open("Turma_B_Nomes.txt", encoding="utf-8")
+Arquivo_Usuários_B = open("Turma_B_Usuários.txt", encoding="utf-8")
+
+Nomes_A = Arquivo_Nomes_A.readlines()
+Usuários_A = Arquivo_Usuários_A.readlines()
+Nomes_B = Arquivo_Nomes_B.readlines()
+Usuários_B = Arquivo_Usuários_B.readlines()
 
 print(Nomes_A)
 print("")
@@ -21,3 +26,27 @@ print("")
 print(Nomes_B)
 print("")
 print(Usuários_B)
+
+Limpa_Nomes_A = []
+
+for p in Nomes_A[1:]:
+    n = p.strip()
+    
+    if n != " ":
+        Limpa_Nomes_A.append(n)
+        
+print(Limpa_Nomes_A)        
+        
+Limpa_Usuários_A = []
+
+for p in Usuários_A[1:]:
+    n = p.strip()
+    
+    if n != " ":
+        Limpa_Usuários_A.append(n)   
+        
+print(Limpa_Usuários_A)          
+        
+        
+    
+

83b442f3fa7e468e6fa0f19d44c9a915bc485cea
thegentil <nicogentil@gmail.com>
2015-05-06 15:10:23 -0300

    Versão 2

--- Projeto Final - Main.py
+++ Projeto Final - Main.py
@@ -2,6 +2,12 @@
 """
 Created on Wed May  6 14:30:22 2015
 
-@author: nicogentil
+@author: Nicolas Gentil, Nicolas Fonteyne, Gabriel Olanda e Felipe Giardini
+
+Projeto Final - Mapa virtual
+
 """
 
+A = open("turmaB.xls", encoding="utf-8")
+
+print(A)

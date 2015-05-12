ad7c6fc79794537493bd2a16c2d2a8d055fa6fee
nicolasakf <nicolasakf@al.insper.edu.br>
2015-05-12 20:28:03 -0300

    VErsão 15

--- Tela_de_login.py
+++ Tela_de_login.py
@@ -10,17 +10,17 @@ Descrição breve: desenvolvimento do aplicativo  INSPERBRAIN
 #=============================================================================#
 """ ABRINDO OS ARQUIVOS E LIMPANDO-OS"""
 
-arquivo_A = open("turma_A.csv")
+arquivo_A = open("turma_A.csv", encoding='latin1')
 arquivo_A = arquivo_A.readlines()
 
-arquivo_limpo_A = []        #limpando o arquivo_A e dando split pelo ponto e vírgula para assim formar uma matriz,
-for e in arquivo_A:         #em que cada um dos elementos é uma lista com o primeiro nome da pessoa, seus sobrenomes e o usuário
+arquivo_limpo_A = []        #limpando o arquivo_A e dando split pelo ponto e virgula para assim formar uma matriz,
+for e in arquivo_A:         #em que cada um dos elementos eh uma lista com o primeiro nome da pessoa, seus sobrenomes e o usuario
     n = e.strip()
     n = n.split(';')
     arquivo_limpo_A.append(n)
 
 
-arquivo_B = open("turma_B.csv")
+arquivo_B = open("turma_B.csv", encoding='latin1')
 arquivo_B = arquivo_B.readlines()
 
 arquivo_limpo_B = []        #mesmo para o arquivo_B
@@ -30,7 +30,7 @@ for e in arquivo_B:
     arquivo_limpo_B.append(n)
 
 
-arquivo_C = open("turma_C.csv")
+arquivo_C = open("turma_C.csv", encoding='latin1')
 arquivo_C = arquivo_C.readlines()
 
 arquivo_limpo_C = []        #mesmo para arquivo_C
@@ -40,7 +40,7 @@ for e in arquivo_C:
     arquivo_limpo_C.append(n)
 
 
-del arquivo_limpo_A[0]      #deletando os primeiros elementos das matrizes arquivo_limpo_A & B que eram desprezíveis
+del arquivo_limpo_A[0]      #deletando os primeiros elementos das matrizes arquivo_limpo_A & B que eram despreziveis
 del arquivo_limpo_B[0]
 del arquivo_limpo_C[0]
 
@@ -50,7 +50,7 @@ del arquivo_limpo_C[0]
 dic_A = {}
 
 for e in arquivo_limpo_A:           #adicionando os elementos do arquivo_limpo_A ao dic_A, sendo a chave o login
-    dic_A[e[2]] = [e[0], e[1]]      #e os valores o primeiro nome e os sobrenomes do usuário
+    dic_A[e[2]] = [e[0], e[1]]      #e os valores o primeiro nome e os sobrenomes do usuario
 
 
 dic_B = {}
@@ -79,19 +79,19 @@ def revisão():          #caso precise printar os dics
 #======================================================================================================================#
 """CRIANDO E RELACIONANDO O INPUT DO USUÁRIO DO PROGRAMA COM OS LOGINS LIDOS PELO PROGRAMA ACIMA"""
 
-def user_verification():            #func de verificação do usuário
+def user_verification():            #func de verificacao do usuario
 
-    tentativas = 0                  #criando a variável que conta quantas vezes o user errou o login
+    tentativas = 0                  #criando a variavel que conta quantas vezes o user errou o login
 
     while True:
 
-        repetir = False             #criando a variável que diz se vai repetir ou não
+        repetir = False             #criando a variavel que diz se vai repetir ou nao
 
         print('')
         user = input('Seu login INSPER: ')
 
         if user in dic_A:
-            print('Bem Vindo ao INSPERBRAIN,\n'+dic_A[user][0])     #dando resposta ao input do usuário
+            print('Bem Vindo ao INSPERBRAIN,\n'+dic_A[user][0])     #dando resposta ao input do usuario
 
         elif user in dic_B:
             print('Bem Vindo ao INSPERBRAIN,\n'+dic_B[user][0])
@@ -101,15 +101,15 @@ def user_verification():            #func de verificação do usuário
 
         else:
             print('USUÁRIO INVÁLIDO')
-            tentativas += 1             #soma um no núemro de tentativas do user de logar
+            tentativas += 1             #soma um no nuemro de tentativas do user de logar
             repetir = True              #faz com que o processo se repita
 
 
-        if repetir == False:        #faz com que o processo pare de se repetir caso o usuário acerte o login
+        if repetir == False:        #faz com que o processo pare de se repetir caso o usuario acerte o login
             break
 
-
-        if tentativas >= 3:         #DAQUI PRA BAIXO É O MODO DE "ESQUECEU SEU LOGIN"
+'''
+        if tentativas >= 3:         #DAQUI PRA BAIXO EH O MODO DE "ESQUECEU SEU LOGIN"
             print('')
             r = input('Esqueceu seu login? ')
 
@@ -139,6 +139,7 @@ def user_verification():            #func de verificação do usuário
 
                 if name_in_dic == 0:
                     print('Desculpe, mas seu nome não consta no cadastro')
+'''
 
 user_verification()
 

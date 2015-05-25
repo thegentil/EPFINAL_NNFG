1a62464da01266027b9647bc6f90da9620425530
gabsolanda <gabs.olanda@gmail.com>
2015-05-25 10:17:08 -0300

    Testando_imagem
    Testando a imagem do calendario no kivy

---
+++ teste_imagem.py
@@ -0,0 +1,10 @@
+import kivy
+from kivy.app import App
+from kivy.uix.image import Image
+from kivy.uix.widget import Widget
+
+class teste_imagem(App):
+    pass
+
+if __name__ == '__main__':
+    teste_imagem().run()

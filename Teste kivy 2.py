d8e151fca06c306ecbf7c425ea2f4472293c2afa
thegentil <nicogentil@gmail.com>
2015-05-26 13:47:52 -0300

    Versão 26

--- Teste kivy 2.py
+++ Teste kivy 2.py
@@ -21,6 +21,22 @@ class Professores(Screen):
 class Sair(Screen):
     pass
 
+class Segunda(Screen):
+    pass
+
+class Terca(Screen):
+    pass
+
+class Quarta(Screen):
+    pass
+
+class Quinta(Screen):
+    pass
+
+class Sexta(Screen):
+    pass
+
+
 class MyScreenManager(ScreenManager):
     pass
 
@@ -37,17 +53,365 @@ MyScreenManager:
     Professores:
     Sair:
 
+    Segunda:
+    Terca:
+    Quarta:
+    Quinta:
+    Sexta:
+
 <Calendario>:
     name: 'Calendario'
 
     BoxLayout:
         orientation: 'vertical'
-        Label:
-            text: 'Calendario'
-            font_size: 30
-            size_hint: [1,.1]
 
         BoxLayout:
+
+            size_hint: [1,.1]
+
+            Button:
+                text: 'Seg'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Segunda'
+            Button:
+                text: 'Ter'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Terca'
+
+            Button:
+                text: 'Qua'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Quarta'
+
+            Button:
+                text: 'Qui'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Quinta'
+
+            Button:
+                text: 'Sex'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Sexta'
+
+        BoxLayout:
+            Button:
+                text: 'Mapa'
+                font_size: 20
+                size_hint: [1,.2]
+                on_release: app.root.current = 'Mapa'
+            Button:
+                text: 'Calendario'
+                font_size: 20
+                size_hint: [1,.2]
+                on_release: app.root.current = 'Calendario'
+
+            Button:
+                text: 'Professores'
+                font_size: 20
+                size_hint: [1,.2]
+                on_release: app.root.current = 'Professores'
+
+            Button:
+                text: 'Sair'
+                font_size: 20
+                size_hint: [1,.2]
+                on_release: app.root.current = 'Sair'
+
+<Segunda>:
+    name: 'Segunda'
+
+    BoxLayout:
+        orientation: 'vertical'
+
+        BoxLayout:
+
+            size_hint: [1,.1]
+
+            Button:
+                text: 'Seg'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Segunda'
+            Button:
+                text: 'Ter'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Terca'
+
+            Button:
+                text: 'Qua'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Quarta'
+
+            Button:
+                text: 'Qui'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Quinta'
+
+            Button:
+                text: 'Sex'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Sexta'
+
+        BoxLayout:
+            Button:
+                text: 'Mapa'
+                font_size: 20
+                size_hint: [1,.2]
+                on_release: app.root.current = 'Mapa'
+            Button:
+                text: 'Calendario'
+                font_size: 20
+                size_hint: [1,.2]
+                on_release: app.root.current = 'Calendario'
+
+            Button:
+                text: 'Professores'
+                font_size: 20
+                size_hint: [1,.2]
+                on_release: app.root.current = 'Professores'
+
+            Button:
+                text: 'Sair'
+                font_size: 20
+                size_hint: [1,.2]
+                on_release: app.root.current = 'Sair'
+
+<Terca>:
+    name: 'Terca'
+
+    BoxLayout:
+        orientation: 'vertical'
+
+        BoxLayout:
+
+            size_hint: [1,.1]
+
+            Button:
+                text: 'Seg'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Segunda'
+            Button:
+                text: 'Ter'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Terca'
+
+            Button:
+                text: 'Qua'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Quarta'
+
+            Button:
+                text: 'Qui'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Quinta'
+
+            Button:
+                text: 'Sex'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Sexta'
+
+        BoxLayout:
+            Button:
+                text: 'Mapa'
+                font_size: 20
+                size_hint: [1,.2]
+                on_release: app.root.current = 'Mapa'
+            Button:
+                text: 'Calendario'
+                font_size: 20
+                size_hint: [1,.2]
+                on_release: app.root.current = 'Calendario'
+
+            Button:
+                text: 'Professores'
+                font_size: 20
+                size_hint: [1,.2]
+                on_release: app.root.current = 'Professores'
+
+            Button:
+                text: 'Sair'
+                font_size: 20
+                size_hint: [1,.2]
+                on_release: app.root.current = 'Sair'
+
+<Quarta>:
+    name: 'Quarta'
+
+    BoxLayout:
+        orientation: 'vertical'
+
+        BoxLayout:
+
+            size_hint: [1,.1]
+
+            Button:
+                text: 'Seg'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Segunda'
+            Button:
+                text: 'Ter'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Terca'
+
+            Button:
+                text: 'Qua'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Quarta'
+
+            Button:
+                text: 'Qui'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Quinta'
+
+            Button:
+                text: 'Sex'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Sexta'
+
+        BoxLayout:
+            Button:
+                text: 'Mapa'
+                font_size: 20
+                size_hint: [1,.2]
+                on_release: app.root.current = 'Mapa'
+            Button:
+                text: 'Calendario'
+                font_size: 20
+                size_hint: [1,.2]
+                on_release: app.root.current = 'Calendario'
+
+            Button:
+                text: 'Professores'
+                font_size: 20
+                size_hint: [1,.2]
+                on_release: app.root.current = 'Professores'
+
+            Button:
+                text: 'Sair'
+                font_size: 20
+                size_hint: [1,.2]
+                on_release: app.root.current = 'Sair'
+
+<Quinta>:
+    name: 'Quinta'
+
+    BoxLayout:
+        orientation: 'vertical'
+
+        BoxLayout:
+
+            size_hint: [1,.1]
+
+            Button:
+                text: 'Seg'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Segunda'
+            Button:
+                text: 'Ter'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Terca'
+
+            Button:
+                text: 'Qua'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Quarta'
+
+            Button:
+                text: 'Qui'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Quinta'
+
+            Button:
+                text: 'Sex'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Sexta'
+
+        BoxLayout:
+            Button:
+                text: 'Mapa'
+                font_size: 20
+                size_hint: [1,.2]
+                on_release: app.root.current = 'Mapa'
+            Button:
+                text: 'Calendario'
+                font_size: 20
+                size_hint: [1,.2]
+                on_release: app.root.current = 'Calendario'
+
+            Button:
+                text: 'Professores'
+                font_size: 20
+                size_hint: [1,.2]
+                on_release: app.root.current = 'Professores'
+
+            Button:
+                text: 'Sair'
+                font_size: 20
+                size_hint: [1,.2]
+                on_release: app.root.current = 'Sair'
+
+<Sexta>:
+    name: 'Sexta'
+
+    BoxLayout:
+        orientation: 'vertical'
+
+        BoxLayout:
+
+            size_hint: [1,.1]
+
+            Button:
+                text: 'Seg'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Segunda'
+            Button:
+                text: 'Ter'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Terca'
+
+            Button:
+                text: 'Qua'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Quarta'
+
+            Button:
+                text: 'Qui'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Quinta'
+
+            Button:
+                text: 'Sex'
+                font_size: 20
+                size_hint: [1,1]
+                on_release: app.root.current = 'Sexta'
 
         BoxLayout:
             Button:
@@ -78,38 +442,36 @@ MyScreenManager:
 
     BoxLayout:
         orientation: 'vertical'
-        Label:
-            text: 'Mapa'
-            font_size: 30
-            size_hint: [1,.1]
 
         BoxLayout:
+            size_hint: [1,1]
             Image:
                 source: 'LAYOUT REVISADO 07-11-page-001.jpg'
                 size_hint: [1,1]
 
         BoxLayout:
+            size_hint: [1,.1]
             Button:
                 text: 'Mapa'
                 font_size: 20
-                size_hint: [1,.2]
+                size_hint: [1,1]
                 on_release: app.root.current = 'Mapa'
             Button:
                 text: 'Calendario'
                 font_size: 20
-                size_hint: [1,.2]
+                size_hint: [1,1]
                 on_release: app.root.current = 'Calendario'
 
             Button:
                 text: 'Professores'
                 font_size: 20
-                size_hint: [1,.2]
+                size_hint: [1,1]
                 on_release: app.root.current = 'Professores'
 
             Button:
                 text: 'Sair'
                 font_size: 20
-                size_hint: [1,.2]
+                size_hint: [1,1]
                 on_release: app.root.current = 'Sair'
 
 <Professores>:
